703a7c45c0a89602cc7a8b03d885238f1ac816be
adam f <afiggat@gmail.com>
2017-08-31 15:00:20 -0600

    completed react and redux drill

--- drill-1/src/App.js
+++ drill-1/src/App.js
@@ -1,23 +1,63 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { addGuest, removeGuest } from './ducks/guestList';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text:''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+    handleInputChange(e) {
+      this.setState({
+        text: e.target.value
+      })
+    }
+    handleSubmit(e) {
+      e.preventDefault();
+      this.props.addGuest(this.state.text);
+      this.setState({
+        test: ''
+      })
+    }
+    
   render() {
     return (
       <div className="App">
         <h1>DevMountain Hackathon</h1>
         <h3>Guest List:</h3>
         <ul>
-          {/*guest list from store*/}
+         {this.props.list.map((guest, i) => {
+           return (
+             <div key={i} className="list-item">
+               <li> {guest} </li>
+               <button type="" className="" onClick={()=>this.props.removeGuest(i)}>Remove</button>
+               </div>
+           )
+         })}
         </ul>
-        <div className="add-guest">
-          Add guest: <input type="" className=""/>
+        <form
+        onSubmit={this.handleSubmit}
+         className="add-guest">
+          Add guest: <input value={this.state.text}
+          onChange={this.handleInputChange}
+          />
           <button type="" className="">Add</button>
-        </div>
+        </form>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    list: state.guests
+  };
+}
+
+export default connect(mapStateToProps, { addGuest, removeGuest })(App);
